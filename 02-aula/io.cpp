6c1e10917bd892f06ab122dee2c50ebc8fd30e00
davicfg <davicfg@hotmail.com>
2020-06-16 18:30:49 -0300

    condição para saber se o paramêtro é "add"

--- 02-aula/io.cpp
+++ 02-aula/io.cpp
@@ -3,18 +3,16 @@
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-  if(argc == 1){
-    std::cout << "Use: " << argv[0] << " add <message>"<< std::endl;
-    return 1;
-  }else if(argc == 2){
+  string acceptedParameter = "add";
+  if (argc == 2 && acceptedParameter == argv[1]) {
     string newMessage;
     std::cin >> newMessage;
     std::cout << newMessage << std::endl;
-  }else if(argc == 3){
+  } else if (argc == 3 && acceptedParameter == argv[1]) {
     string newMessage = argv[2];
     std::cout << newMessage << std::endl;
-  }else{
-    std::cout << "Use: " << argv[0] << " add '<message>' "<< std::endl;
+  } else {
+    std::cout << "Use: " << argv[0] << " add '<message>' " << std::endl;
     return 1;
   }
 
