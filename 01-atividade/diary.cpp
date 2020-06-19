fa9952106fecebd9aa2c37db47109cedf52ef0dd
davicfg <davicfg@gmail.com>
2020-06-19 11:19:13 -0300

    criado arquivo para fazer a atividade 1.

---
+++ 01-atividade/diary.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+void show_help(const std::string &program_name);
+void add_message(const std::string &file_name, const std::string message);
+int list_messages(const std::string &file_name);
+std::string format_current_date(const std::string &format);
+std::string get_current_date();
+std::string get_current_time();
+
+int main(int argc, char const *argv[])
+{
+  std::string diary_file_name = "diary.txt";
+
+  if (argc == 1)
+  {
+    show_help(argv[0]);
+    return 1;
+  }
+
+  std::string add_action = "add";
+  std::string list_action = "list";
+
+  std::string input_action = argv[1];
+
+  if (input_action != add_action and input_action != list_action)
+  {
+    show_help(argv[0]);
+    return 1;
+  }
+
+  if (argv[1] == list_action)
+  {
+    return list_messages(diary_file_name);
+    
+  }
+
+  std::string new_message;
+
+  if (argc == 2)
+  {
+    std::cout << "Informe a mensagem" << std::endl;
+    std::getline(std::cin, new_message);
+
+    add_message(diary_file_name, new_message);
+
+    return 0;
+  }
+  else
+  {
+    new_message = argv[2];
+
+    add_message(diary_file_name, new_message);
+
+    return 0;
+  }
+}
+
+void show_help(const std::string &program_name)
+{
+  std::cout << program_name << " add <message>" << std::endl;
+}
+
+void add_message(const std::string &file_name, const std::string message)
+{
+  std::ofstream diary(file_name, std::ios::app);
+  diary << message << std::endl;
+  std::cout << "Messagem Adicionana" << std::endl;
+}
+
+int list_messages(const std::string &file_name){
+  std::ifstream file;
+
+  file.open(file_name);
+
+  if (!file.is_open()) {
+    std::cerr << "Arquivo não existente ou sem permissão de leitura."
+              << std::endl;
+    return 1;
+  }
+
+  std::string message;
+
+  int line_number = 0;
+
+  while (!file.eof()) {
+    ++line_number;
+    std::getline(file, message);
+
+    if (message.size() == 0) {
+      continue;
+    }
+
+    std::cout << message << std::endl;
+  }
+
+  file.close();
+  return 0;
+}
+
+std::string format_current_date(const std::string &format) {
+  std::time_t time = std::time(nullptr);
+  char result[1024];
+
+  std::strftime(result, sizeof(result), format.c_str(), std::localtime(&time));
+
+  return std::string(result);
+}
+std::string format_current_date(const std::string &format)
+std::string get_current_date() { return format_current_date("%d/%m/%Y"); }
+
+std::string get_current_time() { return format_current_date("%H:%M:%S"); }
