07f10059485a26d1c1cfcfc8d95a9dd853be0c82
davicfg <davicfg@gmail.com>
2020-07-03 20:06:09 -0300

    [WIP]

--- 03-atividade/include/Diary.h
+++ 03-atividade/include/Diary.h
@@ -2,22 +2,22 @@
 #define DIARY_H
 
 #include "Message.h"
-
+#include "Config.h"
 #include <string>
 #include <vector>
 struct Diary
 {
-  Diary(const std::string& filename);
+  Diary(const std::string& filename, const Config config);
   ~Diary();
 
   std::string filename;
   std::vector<Message> messages;
+  Config config;
 
   void add(const std::string& message);
   void add(const Message message);
   void write();
   int load_messages();
-  // void upgrade_messages_array();
   std::vector<Message*> search(const std::string& what);
 
 };
