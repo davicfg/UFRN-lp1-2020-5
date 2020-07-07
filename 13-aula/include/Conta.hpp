78aed0071e3f4923540c97b5e8027383881da99e
davicfg <davicfg@gmail.com>
2020-07-07 20:05:47 -0300

    começando oo com c++

---
+++ 13-aula/include/Conta.hpp
@@ -0,0 +1,20 @@
+#if !defined(CONTA_H)
+#define CONTA_H
+
+#include <string>
+
+class Conta
+{
+public:
+  std::string numero;
+  std::string titular;
+  double saldo;
+  
+  Conta(const std::string &_numero, const std::string &_titular, const double _saldo);
+
+  void sacar(const double &_valor);
+  void depositar(const double &_valor);
+  void transferir(const double &_valor, Conta &_conta);
+};
+
+#endif // CONTA_H
